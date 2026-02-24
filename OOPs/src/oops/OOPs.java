95c08e8db497c60cc92e0e0139f56f866c16d1df
Vaibhavi <Vaibhavi@DESKTOP-7LSG9HE>
2026-02-24 19:56:17 +0530

    firstProgram

--- OOPs/src/oops/OOPs.java
+++ OOPs/src/oops/OOPs.java
@@ -8,13 +8,30 @@ package oops;
  *
  * @author Vaibhavi
  */
+
 public class OOPs {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("hello OOPs");
+        //when we not give value
+        Student s = new Student();
+        System.out.println(s.rno); // 0
+        System.out.println(s.name); //null
+        System.out.println(s.marks); //0.0
+        
+        Student s1 = new Student();
+        s1.rno = 101;
+        s1.name = "vaibhavi";
+        s1.marks = 89.9;
+        System.out.println(s1.rno + " " + s1.name + "  " + s1.marks);
     }
-    
+
+}
+//create class
+//for every single student
+
+class Student {
+
+    int rno;
+    String name;
+    double marks;
 }
